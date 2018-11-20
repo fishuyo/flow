4b5f9c9df3e0e7efcd5711c0b4cae5285475f982
sphere <sphere@interface.1g>
2018-11-19 17:08:47 -0800

    ps mapping

--- data/mappings/phasespace.scala
+++ data/mappings/phasespace.scala
@@ -2,22 +2,24 @@
 
 
 // Phasespace.disconnect
-// Phasespace.connect
+Phasespace.connect
 val ps = new PhasespaceIO
 
 import de.sciss.osc.Message
 val osc = new OSCSink
-osc.connect("Thunder", 9010)
+osc.connect("spherez05", 9010)
 
 // map pickable events for each glove (right/left)
-for(gloveID <- Seq(0,1)){
+for(gloveID <- Seq(1)){
 
 	//point event
   ps.state.map{ case s =>
     val h = s.headPosition
   	val g = s.gloves(gloveID)
     val d = (g.centroid - h).normalized
-    Message("/point", gloveID, h.x,h.y,h.z, d.x,d.y,d.z)
+    val r = Intersect.ray2Allosphere(Ray(h,d))
+    println(r)
+    Message("/point", gloveID, r.o.x,r.o.y,r.o.z, r.d.x,r.d.y,r.d.z)
   } >> osc.sink
   
   //map each fingertip pinch events
@@ -26,13 +28,25 @@ for(gloveID <- Seq(0,1)){
       val h = s.headPosition
       val g = s.gloves(gloveID)
       val d = (g.centroid - h).normalized
+      val r = Intersect.ray2Allosphere(Ray(h,d))
       if(g.isPinchOn(f)){
-        Some(Message("/pick", gloveID, f.id, h.x,h.y,h.z, d.x,d.y,d.z))
+        Some(Message("/pick", gloveID, f.id, r.o.x,r.o.y,r.o.z, r.d.x,r.d.y,r.d.z))
       }else if(g.isPinched(f)){
         val m = g.getPinchTranslation(f)
-        Some(Message("/drag", gloveID, f.id, h.x,h.y,h.z, d.x,d.y,d.z, m.x,m.y,m.z))
+        f match {
+        	// case Led.Index => Some(Message("/translate", gloveID, f.id, r.o.x,r.o.y,r.o.z, r.d.x,r.d.y,r.d.z, m.x,m.y,m.z))
+          case Led.Middle => Some(Message("/rotate", gloveID, f.id, r.o.x,r.o.y,r.o.z, r.d.x,r.d.y,r.d.z, m.x,m.y,m.z))
+          case Led.Ring => 
+          	m *= 0.1f	
+            Some(Message("/scale", gloveID, f.id, r.o.x,r.o.y,r.o.z, r.d.x,r.d.y,r.d.z, m.x,m.y,m.z))
+        	case Led.Pinky => None
+        	case Led.Index => 
+          	m *= (30f*(s.rightGlove.centroid - s.leftGlove.centroid).mag())
+          	Some(Message("/translate", gloveID, 0, r.o.x,r.o.y,r.o.z, r.d.x,r.d.y,r.d.z, m.x,m.y,m.z))
+        }
+        
       }else if(g.isPinchOff(f)){
-        Some(Message("/unpick", gloveID, f.id, h.x,h.y,h.z, d.x,d.y,d.z))
+        Some(Message("/unpick", gloveID, f.id, r.o.x,r.o.y,r.o.z, r.d.x,r.d.y,r.d.z))
       }else None
   	}.filter(_.isDefined).map(_.get) >> osc.sink
  	}
