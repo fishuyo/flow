521cbda5a89697f97d2f48b50f9238a09772630b
Tim Wood <fishuyo@gmail.com>
2018-12-12 05:31:44 -0800

    add seer system sends..

--- server/app/services/Global.scala
+++ server/app/services/Global.scala
@@ -6,6 +6,18 @@ object System {
   var system:ActorSystem = _
   def apply() = system
   def update(s:ActorSystem) = system = s
+
+  def address = System().asInstanceOf[akka.actor.ExtendedActorSystem].provider.getDefaultAddress
+
+  def broadcast(msg:Any) = apply().actorSelection("/user/live.*") ! msg
+  def send(name:String, msg:Any) = apply().actorSelection(s"/user/live.$name.*") ! msg
+  def broadcastRemote(a:Address)(msg:Any) = apply().actorSelection(s"$a/user/live.*") ! msg
+  def sendRemote(a:Address)(name:String, msg:Any) = apply().actorSelection(s"$a/user/live.$name.*") ! msg
+  def broadcastRemotes(as:Seq[Address])(msg:Any) = as.foreach{ case a => apply().actorSelection(s"$a/user/live.*") ! msg}
+  def sendRemotes(as:Seq[Address])(name:String, msg:Any) = as.foreach{ case a => apply().actorSelection(s"$a/user/live.$name.*") ! msg}
+  // def broadcastAll(msg:Any) = remotes.foreach{ case a => apply().actorSelection(s"$a/user/live.*") ! msg }
+  // def sendAll(name:String, msg:Any) = remotes.foreach{ case a => apply().actorSelection(s"$a/user/live.$name.*") ! msg }
+
 }
 
 
