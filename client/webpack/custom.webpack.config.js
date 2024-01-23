bc34dff8cce25c373a5bc4fd84de314d212eba0b
Timothy Wood <fishuyo@gmail.com>
2024-01-23 14:08:43 -0800

    add webpack folder

---
+++ client/webpack/custom.webpack.config.js
@@ -0,0 +1,33 @@
+var merge = require('webpack-merge').merge;
+var generated = require('./scalajs.webpack.config');
+
+var local = {
+    output: {hashFunction : "sha512"},
+    devServer: {
+        historyApiFallback: true
+    },
+    module: {
+        rules: [
+            {
+                test: /\.css$/,
+                use: ['style-loader', 'css-loader']
+            },
+            {
+                test: /\.(ttf|eot|woff|png|glb|svg)$/,
+                use: 'file-loader'
+            },
+            {
+                test: /\.(eot)$/,
+                use: 'url-loader'
+            },
+            {
+                test: /\.(js|mjs)$/,
+                resolve: {
+                    fullySpecified: false
+                }
+            }
+        ]
+    }
+};
+
+module.exports = merge(generated, local);
